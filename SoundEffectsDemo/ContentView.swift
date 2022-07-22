b6a56c628bf754d9f2545dd717afc2050b8bb689
Garth Poitras <411908+gpoitch@users.noreply.github.com>
2022-07-22 10:17:00 -0400

    Initial Commit

---
+++ SoundEffectsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundEffectsDemo
+//
+//  Created by Garth on 7/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundEffectsDemo/SoundEffectsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundEffectsDemoApp.swift
+//  SoundEffectsDemo
+//
+//  Created by Garth on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SoundEffectsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
